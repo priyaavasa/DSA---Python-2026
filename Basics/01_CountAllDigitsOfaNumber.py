96a9ca501960d30b4e9b35fce17ad8b30a8cc215
Priya Vasa <priyasvasa@gmail.com>
2026-04-17 17:49:45 +0000

    Added count digits problem

--- Basics/01_CountAllDigitsOfaNumber.py
+++ Basics/01_CountAllDigitsOfaNumber.py
@@ -1,11 +1,24 @@
+#Brute Force approach
+
 num = 12345
 
-count = 0
+# count = 0
 
-while num > 0:
-    count = count+1
-    n = num%10
-    num = num//10
+# while num > 0:
+#     count = count+1
+#     n = num%10
+#     num = num//10
 
-print(count)
+# print(count)
 
+# Optimal Approach
+
+import math
+
+def counter(num):
+    count = int(math.log10(num)+1)
+
+    return count
+
+result = counter(num)
+print(result)
