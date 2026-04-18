b372f8b1b7cdf2159f0feb0ea5fb0ac6ad323053
Priya Vasa <priyasvasa@gmail.com>
2026-04-18 18:20:53 +0000

    gcd

--- Basics/04_GCD.py
+++ Basics/04_GCD.py
@@ -1,6 +1,8 @@
 n1 = 9
 n2 = 12
 
+# Brute Force
+"""
 def gcd(n1,n2):
     count = 1
     minGcd = min(n1,n2)
@@ -10,6 +12,29 @@ def gcd(n1,n2):
             count =  i
 
     return count
+"""
+
+#Better
+"""
+def gcd(n1,n2):
+    minGcd = min(n1,n2)
+
+    for i in range(minGcd+1,0,-1):
+        if n1%i==0 and n2%i==0:
+
+            return i
+"""
+
+#Optimal
+
+def gcd(n1, n2):
+    if n1 == n2:
+        return n1
+    if n1 > n2:
+        return gcd(n1 - n2, n2)
+    else:
+        return gcd(n1, n2 - n1)
+
 
 result = gcd(n1,n2)
 print(result)
