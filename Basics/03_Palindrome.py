717ba11bdea336e014951eb6b9bf7878346fe78c
Priya Vasa <priyasvasa@gmail.com>
2026-04-18 17:17:58 +0000

    Palindrome

---
+++ Basics/03_Palindrome.py
@@ -0,0 +1,16 @@
+num = 1217
+temp = num
+def palindrome(num):
+    result = 0
+    while num>0:
+        n = num%10
+        result= result*10+n
+        num = num//10
+
+    return result
+
+pal = palindrome(num)
+if(temp == pal):
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
