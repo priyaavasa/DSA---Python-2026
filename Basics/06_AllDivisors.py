5a550fe2bd65fc1095f21128f5dd90f8da1f002f
Priya Vasa <priyasvasa@gmail.com>
2026-04-21 18:18:03 +0000

    optimal divisior

--- Basics/06_AllDivisors.py
+++ Basics/06_AllDivisors.py
@@ -1,5 +1,7 @@
 n = 36
 
+#Brute Force approach
+"""
 def divisors(n):
     res = []
     for i in range(1,n+1):
@@ -7,6 +9,21 @@ def divisors(n):
             res.append(i)
 
     return res
+"""
+#Optimal
+
+import math
+
+def divisors(n):
+    res = []
+    for i in range(1,math.isqrt(n) + 1):
+        if(n%i==0):
+            res.append(i)
+
+        if i != n // i:
+            res.append(n // i)
+    return res
+
 
 result = divisors(n)
 print(result)
