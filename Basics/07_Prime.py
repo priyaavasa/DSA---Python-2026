ff3890ede087dac8179f36d43849a6dc9b691c42
Priya Vasa <priyasvasa@gmail.com>
2026-04-23 18:30:24 +0000

    prime

--- Basics/07_Prime.py
+++ Basics/07_Prime.py
@@ -1,6 +1,6 @@
 #Brute Force
 
-num = 2
+num = 5
 '''
 def prime(num):
     count =0
@@ -14,9 +14,18 @@ def prime(num):
 '''
 
 #Optimal 
+import math
 
 def prime(num):
-    
+    count =0
+    if(num < 2):
+        return false
+    else:
+        for i in range(1, math.isqrt(num)+1):
+            if(num%i==0):
+                count+=1
+
+    return count
 
 result = prime(num)
-print(result>=2)
+print(result==2)
